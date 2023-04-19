fad5ffbc237568a9ecd7f0ce0d70e4916ce66e86
joaoferreira <joaofpinho10@gmail.com>
2023-04-19 21:09:29 +0200

    Start work on fetching data from database

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 import pygame
 
+from database.database_connection_service import DatabaseConnectionService
 
 def game():
+    db_service = DatabaseConnectionService("sqlite:///./atc_game.db")
+
+    db_service.save_all_data()
+
     pygame.init()
 
 
